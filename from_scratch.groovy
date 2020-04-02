4fa3a62c2262cdb87199a092c5a6c4edb01be8e0
gulmira83 <nartaevag@gmail.com>
2020-04-02 00:39:54 -0500

    added

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,38 +1,47 @@
+  
 node {
-    properties([
-        // Below line sets "Discard builds more than 5"
-        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
-
-        //Below line triggers this job every minute
-       pipelineTriggers([pollSCM('* * * * *')])
-       ])
-
-
-stage("Pull Repo"){ 
-    git 'https://github.com/farrukh90/cool_website.git' 
-} 
-
-stage("Install Prerequisites"){
-		sh """
-		ssh centos@jenkins_worker1.gulmiradesign.com                 sudo yum install httpd -y
-		"""
-} 
-stage("Copy atrifacts"){
-    sh """ 
-    scp -r *  centos@jenkins_worker1.gulmiradesign.com:/tmp
-	ssh centos@jenkins_worker1.gulmiradesign.com                 sudo cp -r /tmp/index.html /var/www/html/
-	ssh centos@jenkins_worker1.gulmiradesign.com                 sudo cp -r /tmp/style.css /var/www/html/
-	ssh centos@jenkins_worker1.gulmiradesign.com				   sudo chown centos:centos /var/www/html/
-	ssh centos@jenkins_worker1.gulmiradesign.com				   sudo chmod 777 /var/www/html/*
-	
-	"""
-}
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
 		
-stage("Restart web server"){ 
-   sh "ssh centos@jenkins_worker1.gulmiradesign.com                 sudo systemctl restart httpd"  
-} 
-stage("Slack"){ 
-   slackSend color: '#BADA55', message: 'Hello, World!'  
-  }
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * *')]),
+		parameters([choice(choices: [
+			'dev1.gulmiradesign.com', 
+			'qa1.gulmiradesign.com', 
+			'stage1.gulmiradesign.com', 
+			'prod1.gulmiradesign.com'], 
+			description: 'Please choose an environment', 
+			name: 'ENVIR')]), 
+		])
 
-} 
+		// Pulls a repo from developer
+	stage("Pull Repo"){
+		git   'https://github.com/farrukh90/cool_website.git'
+	}
+		//Installs web server on different environment
+	stage("Install Prerequisites"){
+		sh """
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
+		"""
+	}
+		//Copies over developers files to different environment
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
+		"""
+	}
+		//Restarts web server
+	stage("Restart web server"){
+		sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
+	}
+
+		//Sends a message to slack
+	stage("Slack"){
+		slackSend color: '#BADA55', message: 'Hello, World!'
+	}
+}
