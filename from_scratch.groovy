7affb2cf310b89862b20446de369c68b5434e34b
gulmira83 <nartaevag@gmail.com>
2020-04-02 00:22:56 -0500

    added

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -11,16 +11,28 @@ node {
 stage("Pull Repo"){ 
     git 'https://github.com/farrukh90/cool_website.git' 
 } 
-stage("Stage2"){ 
-echo "hello" 
+
+stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_worker1.gulmiradesign.com                 sudo yum install httpd -y
+		"""
 } 
-stage("Stage3"){ 
-echo "hello" 
+stage("Copy atrifacts"){
+    sh """ 
+    scp -r *  centos@jenkins_worker1.gulmiradesign.com:/tmp
+	ssh centos@jenkins_worker1.gulmiradesign.com                 sudo cp -r /tmp/index.html /var/www/html/
+	ssh centos@jenkins_worker1.gulmiradesign.com                 sudo cp -r /tmp/style.css /var/www/html/
+	ssh centos@jenkins_worker1.gulmiradesign.com				   sudo chown centos:centos /var/www/html/
+	ssh centos@jenkins_worker1.gulmiradesign.com				   sudo chmod 777 /var/www/html/*
+	
+	"""
+}
+		
+stage("Restart web server"){ 
+   sh "ssh centos@jenkins_worker1.gulmiradesign.com                 sudo systemctl restart httpd"  
 } 
-stage("Stage4"){ 
-echo "hello" 
-} 
-stage("Stage5"){ 
-echo "hello" 
-} 
+stage("Slack"){ 
+   slackSend color: '#BADA55', message: 'Hello, World!'  
+  }
+
 } 
