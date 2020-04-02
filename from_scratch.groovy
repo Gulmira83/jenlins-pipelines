d400e4816404aedabb498bf0d0d57e7afe6ad6ce
gulmira83 <nartaevag@gmail.com>
2020-04-01 23:57:37 -0500

    added

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -9,7 +9,7 @@ node {
 
 
 stage("Pull Repo"){ 
-    git https://github.com/farrukh90/cool_website.git 
+    git 'https://github.com/farrukh90/cool_website.git' 
 } 
 stage("Stage2"){ 
 echo "hello" 
