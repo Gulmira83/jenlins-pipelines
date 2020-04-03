403b0d83f3a141bf5a7d73f7c3a87bf3c3901437
gulmira83 <nartaevag@gmail.com>
2020-04-03 15:47:21 -0500

    updated

--- last_build.groovy
+++ last_build.groovy
@@ -1,49 +1,30 @@
-
+  
 node {
-    //keeps only last 5 builds 
-properties([
-    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('H/15 * * * *')),
-    pipelineTriggers([
-        //Runs with "input" Job
-        upstream('Input'),
-        //Execute every minute
-        cron('* * * * *')])
-        
-]} 
-   
-stage("Stage1"){ 
-git 'https://github.com/farrukh90/packer.git' 
-} 
-stage("Stage2"){ 
-echo "hello" 
-} 
-stage("Stage3"){ 
-echo "hello" 
-} 
-stage("Stage4"){ 
-echo "hello" 
-} 
-stage("Stage5"){ 
-node { 
-// some block 
-}  
-} 
-}     
-stage("Stage1"){ 
-checkout([$class: 'GitSCM', branches: [[name: '*/FarrukH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/cool_website.git']]]) 
-} 
-stage("Stage2"){ 
-echo "hello" 
-} 
-stage("Stage3"){ 
-echo "hello" 
-} 
-stage("Stage4"){ 
-echo "hello" 
-} 
-stage("Stage5"){ 
-node { 
-// some block 
-}  
-} 
-} 
+    properties([
+        // Keeps only last 5
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+        pipelineTriggers([
+            // Runs with "Input" Job
+            upstream('Input'),
+            // Execute every minute
+            cron('* * * * *')])
+           
+    ])
+
+	stage("Stage1"){
+		git 'https://github.com/farrukh90/packer.git'
+}
+	stage("Stage2"){
+		echo "hello"
+}
+	stage("Stage3"){
+		echo "hello"
+}
+	stage("Stage4"){
+		echo "hello"
+}
+	stage("Stage5"){
+		node {
+		// some block
+		} 
+	}
