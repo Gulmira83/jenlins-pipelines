0e719378815af248373d9226e7f97ef119377176
gulmira83 <nartaevag@gmail.com>
2020-04-09 15:27:39 -0500

    updated

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -1,122 +1,64 @@
-  
 node {
 	properties(
-		[parameters([
-            choice(choices: 
-				[
+		[parameters(
+			[choice(choices: 
+			[
 				'0.1', 
 				'0.2', 
 				'0.3', 
 				'0.4', 
-				'0.5',
-				'0.6',
-				'0.7',
-				'0.8',
-				'0.9',
-				'10',
-			], 
-		description: 'Which version of the app should I deploy? ', 
-		name: 'Version'), 
-	choice(choices: 
-	[
-		'dev1.gulmiradesign.com', 
-		'qa1.gulmiradesign.com', 
-		'stage1.gulmiradesign.com', 
-		'prod1.gulmiradesign.com'], 
-	description: 'Please provide an environment to build the application', 
-	name: 'ENVIR')])])
-
-
-
-
-
-
-
-		stage("Stage1"){
-			timestamps {
-				ws {
-					checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]]) }
-			}
-		}
-		stage("Get Credentials"){
+				'0.5'], 
+	description: 'Which version of the app should I deploy? ', 
+	name: 'Version')])])
+	stage("Stage1"){
+		timestamps {
+			ws {
+                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])		}
+	}
+} // we are login to ECR 
+	stage("Get Credentials"){
 		timestamps {
 			ws{
 				sh '''
-					aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 679745294409.dkr.ecr.us-east-2.amazonaws.com/artemis
+                aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 679745294409.dkr.ecr.us-east-2.amazonaws.com/artemis
 					'''
-				}
-			}
-		}
-		stage("Build Docker Image"){
+		    }
+	    }
+    }
+    stage("Build Docker Image"){
 		timestamps {
 			ws {
 				sh '''
 					docker build -t artemis:${Version} .
 					'''
+		    }
+	    }
+    }
+
+    stage("Tag Image"){
+		timestamps {
+			ws {
+				sh '''
+                    docker tag artemis:latest 679745294409.dkr.ecr.us-east-2.amazonaws.com/artemis:${Version}
+                    '''
 				}
 			}
 		}
-		stage("Tag Image"){
-			timestamps {
-				ws {
-					sh '''
-						docker tag artemis:latest 679745294409.dkr.ecr.us-east-2.amazonaws.com/artemis:latest
+    stage("Push Image"){
+	    timestamps {
+			ws {
+				sh '''
+					docker push 965334959964.dkr.ecr.us-east-2.amazonaws.com/artemis:${Version}
 					'''
-					}
-				}
-			}
-		stage("Push Image"){
-			timestamps {
-				ws {
-					sh '''
-						docker push 713287746880.dkr.ecr.us-east-2.amazonaws.com/artemis:${Version}
-						'''
 				}
 			}
 		}
-		stage("Send slack notifications"){
-			timestamps {
-				ws {
+	stage("Send slack notifications"){
+		timestamps {
+			ws {
 					echo "Slack"
 					//slackSend color: '#BADA55', message: 'Hello, World!'
 				}
 			}
 		}
-		stage("Authenticate"){
-			timestamps {
-				ws {
-					sh '''
-						ssh centos@${ENVIR} $(aws ecr get-login --no-include-email --region us-east-2)
-						'''
-				}
-			}
-		}
-		stage("Clean Up"){
-			timestamps {
-				ws {
-					try {
-						sh '''
-							#!/bin/bash
-							IMAGES=$(ssh centos@${ENVIR} docker ps -aq) 
-							for i in \$IMAGES; do
-								ssh centos@${ENVIR} docker stop \$i
-								ssh centos@${ENVIR} docker rm \$i
-							done 
-							'''
-					} catch(e) {
-						println("Script failed with error: ${e}")
-						}
-					}
-				}
-			}
-		
-	stage("Run Container"){
-		timestamps {
-			ws {
-				sh '''
-					ssh centos@${ENVIR} docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-2.amazonaws.com/artemis:${Version}
-					'''
-				}
-			}
-		}
-}
+	}
